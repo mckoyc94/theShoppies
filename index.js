875200bc62072c0fde1ae1a6939c0f4dae6596de
Cmac <mckoyc@gmail.com>
2021-05-08 19:03:03 -0400

    set up ajax call and got reliable responses from API

--- index.js
+++ index.js
@@ -5,5 +5,20 @@ const nomList = $('#nomination')
 
 searchBar.on('input', () => {
     searchStat.text(searchBar.val())
+    const movie = searchBar.val()
+    const query = "https://www.omdbapi.com/?s=" + movie + "&apikey=trilogy"
+
+    $.ajax({
+        method: "GET",
+        url: query
+    }).then(data => {
+        if(data.Search === undefined){
+            console.log('No Search Obj')
+        } else {
+            for(var i = 0; i < 3; i++){
+                console.log(data.Search[i])
+            }
+        }
+    })
 })
 
